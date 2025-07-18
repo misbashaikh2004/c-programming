2881aaa220d7799c918ba4c1cc3d511bbbb8008a
misbashaikh2004 <misba6602@gmail.com>
2025-07-18 22:09:04 +0530

    First commit

---
+++ character.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+    char ch;
+    printf("Enter Anything:");
+    scanf("%c",&ch);
+    if(isupper(ch))
+    printf("Upper character");
+    else if(islower(ch))
+    printf("Lower character");
+else if(isdigit(ch))
+printf("digit");
+else
+printf("Special symbol");
+return 0;
+}
