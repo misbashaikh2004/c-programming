63b77ab9c2fbaafb1e31d474361e9b18275f324d
misbashaikh2004 <misba6602@gmail.com>
2025-07-22 11:15:01 +0530

    First commit

---
+++ month.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int month_no;
+    printf("Enter the Month no:");
+    scanf("%d",&month_no);
+    switch(month_no)
+    {
+        case 1:printf("31 Days");
+        break;
+        case 2:printf("28 Days");
+        break;
+        case 3:printf("31 Days");
+        break;
+        case 4:printf("30 Days");
+        break;
+        case 5:printf("31 Days");
+        break;
+        case 6:printf("30 Days");
+        break;
+        case 7:printf("31 Days");
+        break;
+        case 8:printf("30 Days");
+        break;
+        case 9:printf("31 Days");
+        break;
+        case 10:printf("30 Days");
+        break;
+        case 11:printf("31 Days");
+        break;
+        case 12:printf("30 Days");
+        break;
+        default:
+        printf("Invalid");
+    }
+return 0;
+}
